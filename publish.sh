13d837521071b186f870a4e54ac1540b78f098f7
秋知 <benfchen.cf@alibaba-inc.com>
2014-09-18 20:51:02 +0800

    1.2.1

--- publish.sh
+++ publish.sh
@@ -4,12 +4,14 @@ cd $1
 
 if $3
 then
+	echo '333'
 	git add --all
 	git commit -am $3
 	git tag publish/$2
 	git push origin master:daily/$2
 	git push origin publish/$2:publish/$2
 else
+	echo '222'
 	git add --all
 	git commit -am $2
 	git tag publish/$2
