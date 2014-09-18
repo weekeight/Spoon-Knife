90e9c2b9f3bee5d75c4841ea29c7e3cbcdc6efe9
秋知 <benfchen.cf@alibaba-inc.com>
2014-09-18 20:50:25 +0800

    1.1.1

--- publish.sh
+++ publish.sh
@@ -2,10 +2,19 @@
 
 cd $1
 
-git add --all
-git commit -am $2
-git tag publish/$2
-git push origin master:daily/$2
-git push origin publish/$2:publish/$2
+if $3
+then
+	git add --all
+	git commit -am $3
+	git tag publish/$2
+	git push origin master:daily/$2
+	git push origin publish/$2:publish/$2
+else
+	git add --all
+	git commit -am $2
+	git tag publish/$2
+	git push origin master:daily/$2
+	git push origin publish/$2:publish/$2
+fi
 
 echo 'publish done...';
