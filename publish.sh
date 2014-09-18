22a4d3dfda8a165c0f353374e0ef7fe9f8c7c99f
秋知 <benfchen.cf@alibaba-inc.com>
2014-09-18 21:00:10 +0800

    hahaha

--- publish.sh
+++ publish.sh
@@ -2,20 +2,17 @@
 
 cd $1
 
+git add --all
+
 if [ $# -eq 3 ];then
-	echo '333'
-	git add --all
 	git commit -am $3
-	git tag publish/$2
-	git push origin master:daily/$2
-	git push origin publish/$2:publish/$2
+	
 else
-	echo '222'
-	git add --all
 	git commit -am $2
-	git tag publish/$2
-	git push origin master:daily/$2
-	git push origin publish/$2:publish/$2
 fi
+
+git tag publish/$2
+git push origin master:daily/$2
+git push origin publish/$2:publish/$2
 
 echo 'publish done...';
